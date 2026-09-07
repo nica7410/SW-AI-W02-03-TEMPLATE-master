62950ac1d2f3dd63a32ac82b6c65f076bf1b9bcf
nica7410 <69440845+nica7410@users.noreply.github.com>
2026-09-07 19:37:48 +0900

    W3 basic 문제 2회차 완료

--- week3/1. basic/01_binary_tree.py
+++ week3/1. basic/01_binary_tree.py
@@ -44,17 +44,20 @@ def preorder(root):
     result = []
     
     # TODO: root가 None이면 빈 리스트 반환
-    pass
+    if root == None : return []   
     
     # TODO: 루트 값 추가
-    pass
+    result.append(root.value)
+    #pass
     
     # TODO: 왼쪽 서브트리 순회
-    pass
+    result += preorder(root.left)
+    #pass
     
     # TODO: 오른쪽 서브트리 순회
-    pass
-    
+    result += preorder(root.right)
+    #pass
+
     return result
 
 def inorder(root):
@@ -62,16 +65,20 @@ def inorder(root):
     result = []
     
     # TODO: root가 None이면 빈 리스트 반환
-    pass
+    if root == None : return [] 
+    #pass
     
     # TODO: 왼쪽 서브트리 순회
-    pass
+    result += inorder(root.left)
+    #pass
     
     # TODO: 루트 값 추가
-    pass
+    result.append(root.value)
+    #pass
     
     # TODO: 오른쪽 서브트리 순회
-    pass
+    result += inorder(root.right)
+    #pass
     
     return result
 
@@ -80,16 +87,20 @@ def postorder(root):
     result = []
     
     # TODO: root가 None이면 빈 리스트 반환
-    pass
+    if root == None : return [] 
+    #pass
     
     # TODO: 왼쪽 서브트리 순회
-    pass
+    result += postorder(root.left)
+    #pass
     
     # TODO: 오른쪽 서브트리 순회
-    pass
+    result += postorder(root.right)
+    #pass
     
     # TODO: 루트 값 추가
-    pass
+    result.append(root.value)
+    #pass
     
     return result
 
