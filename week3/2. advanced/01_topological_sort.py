88a3ee7c56ceabda9b77d0186f24e8ecf10c8fc4
nica7410 <69440845+nica7410@users.noreply.github.com>
2026-09-08 21:28:48 +0900

    W3 advanced 문제 3회차 완료

--- week3/2. advanced/01_topological_sort.py
+++ week3/2. advanced/01_topological_sort.py
@@ -40,20 +40,40 @@ def topological_sort(vertices, edges):
         위상 정렬 순서
     """
     # TODO: 그래프와 진입 차수 초기화
-    pass
+    graph = {i: [] for i in range(vertices)}
+    indegree = [0] * vertices
+    #pass
     
     # TODO: 그래프 구성 및 진입 차수 계산
-    pass
+    for u,v in edges :
+        graph[u].append(v)
+        indegree[v] += 1
+    queue = deque()
+    #pass
     
     # TODO: 진입 차수가 0인 정점들을 큐에 추가
-    pass
+    for i in range(vertices) :
+        if indegree[i] == 0 : 
+            queue.append(i)
+    #pass
     
     result = []
     
     # TODO: 큐가 빌 때까지 반복
     ## 큐에서 정점 꺼내기
     ## 인접한 정점들의 진입 차수 감소
-    pass
+    while queue :
+        current = queue.popleft()        
+        result.append(current)
+        for next_vertex in graph[current]:
+            indegree[next_vertex] -= 1
+
+            if indegree[next_vertex] == 0 :
+                queue.append(next_vertex)
+
+    if len(result) != vertices:
+        return []
+    #pass
     
     return result
 
